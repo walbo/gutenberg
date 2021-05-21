e909cf322cadd53a3a71d6d317b4cd7bf3cc14ea
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-05-21 16:48:23 +1000

    Set border style to none when 0 width
    This application of the border-style: none will only occur if border style support has been opted into for the block.

--- packages/block-editor/src/hooks/border-width.js
+++ packages/block-editor/src/hooks/border-width.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __experimentalUnitControl as UnitControl } from '@wordpress/components';
-import { useState } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -25,10 +25,21 @@ export const BorderWidthEdit = ( props ) => {
 		setAttributes,
 	} = props;
 
+	const { width, style: borderStyle } = style?.border || {};
+
 	// Step value is maintained in state so step is appropriate for current unit
 	// even when current radius value is undefined.
-	const initialStep = parseUnit( style?.border?.width ) === 'px' ? 1 : 0.25;
+	const initialStep = parseUnit( width ) === 'px' ? 1 : 0.25;
 	const [ step, setStep ] = useState( initialStep );
+	const [ styleSelection, setStyleSelection ] = useState();
+
+	// Temporarily track previous border style selection to be able to restore
+	// it when border width changes from zero value.
+	useEffect( () => {
+		if ( borderStyle !== 'none' ) {
+			setStyleSelection( borderStyle );
+		}
+	}, [ borderStyle ] );
 
 	const onUnitChange = ( newUnit ) => {
 		setStep( newUnit === 'px' ? 1 : 0.25 );
@@ -43,16 +54,34 @@ export const BorderWidthEdit = ( props ) => {
 			},
 		};
 
+		const hasZeroWidth = parseFloat( newWidth ) === 0;
+
+		// Setting the border width explicitly to zero will also set the
+		// border style prop to `none`. This style will only be applied if
+		// border style support has also been opted into.
+		if ( hasZeroWidth ) {
+			newStyle.border.style = 'none';
+		}
+
+		// Restore previous border style selection if width is now not zero and
+		// border style was 'none'. This is to support changes to the UI which
+		// change the border style UI to a segmented control without a "none"
+		// option.
+		if ( ! hasZeroWidth && borderStyle === 'none' ) {
+			newStyle.border.style = styleSelection;
+		}
+
+		// If width was reset, clean out undefined styles.
 		if ( newWidth === undefined || newWidth === '' ) {
 			newStyle = cleanEmptyObject( newStyle );
 		}
 
-		setAttributes( { style: cleanEmptyObject( newStyle ) } );
+		setAttributes( { style: newStyle } );
 	};
 
 	return (
 		<UnitControl
-			value={ style?.border?.width }
+			value={ width }
 			label={ __( 'Border width' ) }
 			min={ MIN_BORDER_WIDTH }
 			onChange={ onChange }
