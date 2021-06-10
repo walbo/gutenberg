10c98d2d8c94646790fc75f464b2b157a3f43e72
Ella van Durpe <ella@vandurpe.com>
2021-06-10 19:13:48 +0300

    Also move front-end styles

--- lib/block-supports/layout.php
+++ lib/block-supports/layout.php
@@ -86,7 +86,14 @@ function gutenberg_render_layout_support_flag( $block_content, $block ) {
 		1
 	);
 
-	return $content . '<style>' . $style . '</style>';
+	add_action(
+		'wp_head',
+		function () use ( $style ) {
+			echo '<style>' . $style . '</style>';
+		}
+	);
+
+	return $content;
 }
 
 // Register the block support.
