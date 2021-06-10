40b7691824d6732357cdc7746cdba1525f59c59c
ntsekouras <ntsekouras@outlook.com>
2021-06-10 17:12:16 +0300

    follow up

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -105,7 +105,7 @@ export { default as RangeControl } from './range-control';
 export { default as ResizableBox } from './resizable-box';
 export { default as ResponsiveWrapper } from './responsive-wrapper';
 export { default as SandBox } from './sandbox';
-export { default as __experimentalSegmentedControl } from './segmented-control';
+export { SegmentedControl as __experimentalSegmentedControl } from './segmented-control';
 export { default as SelectControl } from './select-control';
 export { default as Snackbar } from './snackbar';
 export { default as SnackbarList } from './snackbar/list';
